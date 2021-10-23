6448921784a62c883d4c2c8eb19176f425303e78
surajiyer283 <surajiyer28.si@gmail.com>
2021-10-23 17:47:42 +0530

    login done

--- app/src/main/java/com/firstapp/myapplication/LoginActivity.java
+++ app/src/main/java/com/firstapp/myapplication/LoginActivity.java
@@ -1,18 +1,33 @@
 package com.firstapp.myapplication;
 
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+import java.util.Properties;
 
 public class LoginActivity extends AppCompatActivity {
     Button b1,b2;
     EditText ed1,ed2;
+    FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,26 +36,32 @@ public class LoginActivity extends AppCompatActivity {
         b2 = (Button)findViewById(R.id.register);
         ed1 = (EditText)findViewById(R.id.username);
         ed2 = (EditText)findViewById(R.id.password);
+        db = FirebaseFirestore.getInstance();
 
 
 
     }
 
     public void login(View view) {
-        if(ed1.getText().toString().equals("aanch") &&
-                ed2.getText().toString().equals("aanch")) {
-            Toast.makeText(getApplicationContext(),
-                    "Login Success",Toast.LENGTH_SHORT).show();
-            Intent Login = new Intent(LoginActivity.this, GroupListActivity.class);
-            startActivity(Login);
+        db.collection("user").whereEqualTo("username",ed1.getText().toString()).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> user = document.getData();
+                                if(user.get("password").equals(ed2.getText().toString())){
+                                    Intent homepage = new Intent(LoginActivity.this, GroupListActivity.class);
+                                    startActivity(homepage);
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }
+                });
 
-        }else{
-            Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-
-
-
-
-        }
     }
 
     public void register(View view) {
--- app/src/main/java/com/firstapp/myapplication/RegisterActivity.java
+++ app/src/main/java/com/firstapp/myapplication/RegisterActivity.java
@@ -4,14 +4,12 @@ package com.firstapp.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,10 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
-    Button button;
-    ImageView imageView;
-    int PICTURE = 100;
-    Drawable d = null;
+
     EditText na,uname,email,pwd,cpwd;
     FirebaseFirestore db;
 
@@ -40,7 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
         pwd = (EditText) findViewById(R.id.password);
         cpwd = (EditText) findViewById(R.id.cpassword);
 
-        imageView = findViewById(R.id.imageview);
+
 
         db = FirebaseFirestore.getInstance();
 
@@ -53,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
         String emailid = email.getText().toString();
         String pass = pwd.getText().toString();
         String cpass = cpwd.getText().toString();
-        Drawable img = imageView.getDrawable();
+
         if(name.equals("") || username.equals("") || emailid.equals("") || pass.equals("") || cpass.equals("")) {
             Toast.makeText(getApplicationContext(), "All fields are required.",Toast.LENGTH_SHORT).show();
         }
@@ -61,9 +56,6 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Passwords do not match.",Toast.LENGTH_SHORT).show();
         }
 
-        else if(img == null){
-            Toast.makeText(getApplicationContext(), "Profile image is required.",Toast.LENGTH_SHORT).show();
-        }
 
         else{
             Map<String,Object> user = new HashMap<>();
@@ -92,25 +84,5 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    public void getImage(View view) {
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), PICTURE);
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            if (requestCode == PICTURE) {
-                Uri selectedImageUri = data.getData();
-                if (null != selectedImageUri) {
-                    imageView.setImageURI(selectedImageUri);
-                }
-            }
-        }
-    }
 }
